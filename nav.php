eff8b2a0148e50117ec77f28247406d5b36af3d2
9vranasinghe <137360420+9vranasinghe@users.noreply.github.com>
2023-09-08 14:33:38 +1200

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,19 +1,33 @@
+<!---- Navigation bar and the title ----->
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="style_nav.css">
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style_nav.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> 
+	<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Expletus+Sans&family=Orbitron&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="designers.php">Designers</a></li>
-            <li><a href="category.php">Category</a></li>
-            <li><a href="clothing.php">Clothing</a></li>
-            <li><a href="non-clothing.php">Non-clothing</a></li>
-            <li><a href="images.php">Images</a></li>
-        </ul>
-    </nav>
+    <div class="navbar">
+        <h1>Kool Kiwiana</h1>
+		<a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <div class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Products <i class="material-icons">keyboard_arrow_down</i></a>
+            <div class="dropdown-content">
+                <a href="all_products.php">All Products</a>
+                <a href="clothing.php">Clothing</a>
+                <a href="non-clothing.php">Non-Clothing</a>
+            </div>
+        </div>
+        <a href="contact.php">Contact</a>
+    </div>
 </body>
 </html>
+
