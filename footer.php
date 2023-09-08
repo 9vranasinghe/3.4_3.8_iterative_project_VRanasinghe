c4cb043c6b568809cf36bbd4a1abc7dadea070b7
9vranasinghe <137360420+9vranasinghe@users.noreply.github.com>
2023-09-08 14:37:53 +1200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- The footer of the website -->
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<footer>
+    <p>&copy; Copyright Viduka Thejan Ranasinghe, all rights reserved, Tawa College 2023</p>
+    <p2><a href="index.php">Home</a></p2>
+</footer>
